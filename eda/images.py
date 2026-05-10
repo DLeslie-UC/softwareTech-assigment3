8bf30af4e75f2fc8dfe842f7949ad9406f894195
DL <265407563+DLeslie-UC@users.noreply.github.com>
2026-05-10 22:21:50 +1000

    made the gui work with the eda class done so far. did more work on the eda class to add the summary table and made that show up in the gui, albeit it doesn't look the nicest. Need to add image processing and grid of images stuff

--- eda/images.py
+++ eda/images.py
@@ -1,22 +1,21 @@
 import os
 from math import sqrt
-import cv2
+from PIL import Image
 import pandas as pd
 
+# TODO: Change to take a list of species and handle the logic there
+# Need to pass in functions to a function maybe?
 class edaData():
 
-    def __init__(self, species: list[str]):
-        self.species = species
-        # NOTE: likely need to change to a third party class available top level that sets it automatically
+    def __init__(self):
+        # self.species = species
         self.__dataset_path = f"{os.getcwd()}/insects_dataset"
 
-    # NOTE: Could probably extract this to its own library
     def __folder_check(self, species: str) -> tuple[list[str], str] | FileNotFoundError:
         try:
             path = f"{self.__dataset_path}/{species}"
             image_list = os.listdir(path)
         except FileNotFoundError:
-            # NOTE: Likely need to change to a return to allow the gui to show it
             raise FileNotFoundError(f"The Species, {species},  doesn't exist in the dataset path")
         return image_list, path
 
@@ -36,11 +35,9 @@ class edaData():
         image_count = self.number_of_image(species, image_list)
         for image in image_list:
             image_path = f"{path}/{image}"
-            # (height, width)
-            result = cv2.imread(image_path).shape[:2]
-            # print(result)
-            height_total += result[0]
-            width_total += result[1]
+            width, height = Image.open(image_path).size
+            height_total += height
+            width_total += width
         height_mean = round(height_total / image_count, 2)
         width_mean = round(width_total / image_count, 2)
 
@@ -56,17 +53,16 @@ class edaData():
 
         for image in image_list:
             image_path = f"{path}/{image}"
-            # (height, width)
-            result = cv2.imread(image_path).shape[:2]
-            height_sum_squared_diff += (result[0] - height_mean)**2
-            width_sum_squared_diff += (result[1] - width_mean)**2
+            width, height = Image.open(image_path).size
+            height_sum_squared_diff += (height - height_mean)**2
+            width_sum_squared_diff += (width - width_mean)**2
 
         height_std = round(sqrt(height_sum_squared_diff/image_count), 2)
         width_std = round(sqrt(width_sum_squared_diff/image_count), 2)
 
         return height_std, width_std
 
-    def representative_images(self, species: str) -> list[str]: 
+    def representative_images(self, species: str, num_per_species: int = 9) -> list[str]: 
         ''' Gets a representative set of images for the species by checking if the images for it are within +-1 standard deviation for the species images set '''
         image_list, path = self.__folder_check(species)
         height_mean, width_mean = self.mean_height_width(species)
@@ -78,32 +74,49 @@ class edaData():
         representative_list = []
         for image in image_list:
             image_path = f"{path}/{image}"
-            result = cv2.imread(image_path).shape[:2]
-            height = result[0]
-            width = result[1]
+            width, height = Image.open(image_path).size
             height_check = height_lower_bound <= height <= height_upper_bound
             width_check = width_lower_bound <= width <= width_upper_bound
             if (height_check) and (width_check):
                 representative_list.append(image)
             # NOTE: Can change len req if need more images
             # Done to check if have enough images already
-            if len(representative_list) > 9:
+            if len(representative_list) > num_per_species:
                 break
         return representative_list
 
+    def summary_table(self, species_list: list[str]) -> pd.DataFrame:
+        species_dict = {}
+        number = 0
+        for species in species_list:
+            number += 1
+            image_list, path = self.__folder_check(species)
+            height_mean, width_mean = self.mean_height_width(species)
+            total_images = self.number_of_image(species, image_list)
+            species_dict[species] = {"Number": number, "Height Mean": height_mean, "Width Mean": width_mean, "Total Images": total_images}
+        species_table = pd.DataFrame.from_dict(species_dict, orient='index')
+        return species_table
+
 def main():
-    eg = edaData(['Limnius sp', 'Asellus sp'])
-    # eg = edaData(['Limnius sp'])
-    for species in eg.species:
-        # print(f"Species: {species} has {eg.number_of_image(species)} number of images")
-        # height_mean, width_mean = eg.mean_height_width(species)
-        # print(f"Species: {species} has height mean: {height_mean} and width mean: {width_mean}")
-        # height_std, width_std = eg.standard_deviation(species)
-        # print(f"Species: {species} has height std: {height_std} and width std: {width_std}")
-        # height_iqr, width_iqr = eg.iqr(species)
-        # print(f"Species: {species} has height iqr: {height_iqr} and width iqr: {width_iqr}")
-        representative_list = eg.representative_images(species)
-        print(f"Species: {species} has representative images: {representative_list}")
+    # eg = edaData(['Limnius sp', 'Asellus sp'])
+    species_list = ['Limnius sp', 'Asellus sp']
+    eg = edaData()
+    species_df = eg.summary_table(species_list)
+    print(f"Species df: \n{species_df}")
+    print(f"Species rows: \n{species_df.index.values}")
+    print(f"Species columns: \n{species_df.columns.values}")
+    for species in species_list:
+        print("")
+        for column in species_df.columns.values:
+            # print(f"{species} value for {column}: {species_df.loc[species][column]}")
+            print(f"{species} value for {column}: {species_df.loc[species]}")
+    #     print(f"Species: {species} has {eg.number_of_image(species)} number of images")
+    #     height_mean, width_mean = eg.mean_height_width(species)
+    #     print(f"Species: {species} has height mean: {height_mean} and width mean: {width_mean}")
+    #     height_std, width_std = eg.standard_deviation(species)
+    #     print(f"Species: {species} has height std: {height_std} and width std: {width_std}")
+    #     representative_list = eg.representative_images(species)
+    #     print(f"Species: {species} has representative images: {representative_list}")
 
 
 if __name__ == "__main__":
